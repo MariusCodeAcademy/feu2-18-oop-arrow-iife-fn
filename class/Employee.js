43448c647e8073125eee568809eb75799724a709
Marius <marius@example.com>
2022-11-04 15:45:21 +0200

    work

--- class/Employee.js
+++ class/Employee.js
@@ -6,7 +6,15 @@ class Employee {
     // darbo valandoms apskaityti reiksme
     this.hoursWorked = 0;
   }
+
+  work(howManyHours) {
+    console.log(`${this.firsName} worked for ${howManyHours} hours`);
+    this.hoursWorked += howManyHours;
+  }
 }
 
 const em1 = new Employee('James', 'Bond', 10);
 console.log('em1 ===', em1);
+em1.work(10);
+em1.work(50);
+console.log('em1.hoursWorked ===', em1.hoursWorked);
