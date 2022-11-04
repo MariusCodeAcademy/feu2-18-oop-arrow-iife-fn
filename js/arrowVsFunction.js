59ca00e7703e8b1a01143c1668d125595fc45e91
Marius <marius@example.com>
2022-11-04 08:58:15 +0200

    failai

---
+++ js/arrowVsFunction.js
@@ -0,0 +1,4 @@
+'use strict';
+console.log('arrowVsFunction.js');
+
+// arrow ir kitu funkciju skirtumas
---
+++ js/iife.js
@@ -0,0 +1,4 @@
+'use strict';
+console.log('iife.js');
+
+// imediately invoked function expresion
