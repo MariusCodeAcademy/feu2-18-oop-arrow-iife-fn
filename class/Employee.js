9d3125c5f5665575ba0db1f5d2367fe39dc5fd82
Marius <marius@example.com>
2022-11-04 15:54:43 +0200

    calcPay

--- class/Employee.js
+++ class/Employee.js
@@ -11,10 +11,24 @@ class Employee {
     console.log(`${this.firsName} worked for ${howManyHours} hours`);
     this.hoursWorked += howManyHours;
   }
+
+  calcPay() {
+    // skirtas atvaizduoti dabartini atlyginima
+    const pay = this.hoursWorked * this.hourlyPay;
+    console.log(`${this.firsName} salary is ${pay}`);
+    return pay;
+  }
 }
 
 const em1 = new Employee('James', 'Bond', 10);
+const em2 = new Employee('Hana', 'Smith', 15);
 console.log('em1 ===', em1);
+em2.work(10);
+em2.work(100);
 em1.work(10);
 em1.work(50);
 console.log('em1.hoursWorked ===', em1.hoursWorked);
+console.log('em2.hoursWorked ===', em2.hoursWorked);
+
+console.log('em1.calcPay() ===', em1.calcPay());
+console.log('em2.calcPay() ===', em2.calcPay());
